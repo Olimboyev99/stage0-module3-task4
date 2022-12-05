1adb18baeb71c7b09f583781a0054b60d0646633
Hasanboy <hasanboyolimboyev40@gmail.com>
2022-12-05 12:39:10 +0500

    good

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -1,6 +1,13 @@
 package lang.print.gaps.task4;
 
+import java.util.Scanner;
+
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a,b;
+        Scanner in = new Scanner(System.in);
+        a=in.nextInt();
+        b=in.nextInt();
+        double c=(9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,17 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char a='c';
+        char b='h';
+        char c='a';
+        char d='r';
+        int e=a;
+        int f=b;
+        int g=c;
+        int h=e;
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
     }
 }
